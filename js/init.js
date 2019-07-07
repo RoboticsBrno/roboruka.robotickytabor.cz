45857b72973c2a6f29b6e6187176a1e8511765e0
Jan Mrázek <email@honzamrazek.cz>
2019-07-07 17:52:28 +0200

    Add automatic refresh on guide change

--- js/init.js
+++ js/init.js
@@ -1,11 +1,25 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.carousel').carousel();
-    $('#nav-mobile a').click(function() {
-      $("#sidenav-overlay").click();
-    });
+        $('.button-collapse').sideNav();
+        $('.carousel').carousel();
+        $('#nav-mobile a').click(function () {
+            $("#sidenav-overlay").click();
+        });
 
-  }); // end of document ready
+        var currentPageVersion;
+        $.get('/version', function(data) {
+            currentPageVersion = data;
+            setInterval(function () {
+                $.get('/version', function(data) {
+                    if (data != currentPageVersion) {
+                        currentPageVersion = data;
+                        if (confirm('Nová verze návodu k dispozici, aktualizovat?')) {
+                            location.reload(true);
+                        }
+                    }
+                });
+            }, 3 * 60 * 1000);
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
