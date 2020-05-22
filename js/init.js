39c14081b4eab6387f776501fb09d106d88ecc46
Vojtech Bocek <vbocek@gmail.com>
2020-05-22 22:55:47 +0200

    Step navigation improvements

--- js/init.js
+++ js/init.js
@@ -1,4 +1,26 @@
+
 (function ($) {
+    var initSteps = function() {
+        var globalCounter = 0;
+        document.querySelectorAll("p.step, .con-guide hr").forEach(function(el) {
+            if(el.tagName == "HR") {
+                ++globalCounter;
+                return;
+            }
+
+            var globalStep = "";
+            if(globalCounter > 26) {
+                globalStep = String.fromCharCode(64 + globalCounter/26, 65 + globalCounter%26);
+            } else {
+                globalStep = String.fromCharCode(64 + globalCounter);
+            }
+
+            var step = globalStep + "-" + el.textContent;
+            el.innerHTML = "Krok " + step;
+            el.parentElement.parentElement.id = "step-" + step;
+        });
+    };
+
     $(function () {
 
         $('.button-collapse').sideNav();
@@ -27,5 +49,48 @@
                 return $(this).next('figcaption').html();
             }
         });
+
+        initSteps();
+
+        document.addEventListener("scroll", onDocumentScroll);
+
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+function onJumpToStepClick() {
+    var res = window.prompt("Zadejte číslo kroku (příklad: A-1, K-12, s1, ...)")
+    if(res === null) {
+        return
+    }
+
+    var m = /([a-zA-Z]+)[\s-]*([0-9]+)/.exec(res)
+    if(m == null) {
+        return;
+    }
+
+    var id = "step-" + m[1].toUpperCase() + "-" + m[2];
+    document.getElementById(id).scrollIntoView();
+}
+
+var scrollReactionTimeout = null;
+
+function onDocumentScroll() {
+    if(scrollReactionTimeout !== null) {
+        clearTimeout(scrollReactionTimeout);
+    }
+    scrollReactionTimeout = setTimeout(onScrollFinished, 300);
+}
+
+function onScrollFinished() {
+    var list = document.querySelectorAll("figure[id^=\"step-\"]");
+    var len = list.length;
+    for(var i = 0; i < len; ++i) {
+        var el = list[i];
+        var bounding = el.getBoundingClientRect();
+        if(bounding.top >= 0) {
+            history.replaceState(null, null, '#' + el.id);
+            break;
+        }
+    }
+}
